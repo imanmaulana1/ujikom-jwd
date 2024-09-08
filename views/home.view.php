3ac1e0db69735304d9e99032c44278457c74a10c
Iman Maulana <maulanaiman902@gmail.com>
2024-09-09 03:58:47 +0700

    feat: add home section

--- views/home.view.php
+++ views/home.view.php
@@ -1,1 +1,73 @@
-<h1 class="text-danger">Home</h1>
+<section class="hero">
+    <div class="container">
+        <div class="mt-5 row g-2 align-items-center">
+            <div class="col-12 col-lg-6">
+                <h1>
+                    Bergabunglah dalam Program Beasiswa Kami
+                </h1>
+                <p>Temukan peluang beasiswa yang dapat membantu Anda mencapai tujuan akademis dan profesional.</p>
+                <a href="registration_form.php" class="btn btn-primary">Gabung Sekarang</a>
+            </div>
+            <div class="col-12 col-lg-6 d-none d-md-block">
+                <div class="img-wrapper">
+                    <img src="./assets/images/scholarship.png" alt="hero">
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<section class="my-5 py-4">
+    <div class="container">
+        <h2 class="text-center">Beasiswa</h2>
+        <div class="container-narrow">
+            <div class="mt-4 row">
+                <div class="col-12 col-lg-6">
+                    <div class="card py-2">
+                        <h3 class="subtitle text-center">Akademik</h3>
+                        <div class="card-body">
+                            <p><strong>Deskripsi:</strong></p>
+                            <p>Beasiswa Akademik dirancang untuk mendukung mahasiswa yang memiliki prestasi akademis yang luar biasa. Program ini bertujuan untuk memberikan bantuan keuangan kepada siswa yang menunjukkan dedikasi tinggi terhadap studi mereka.</p>
+                            <p><strong>Persyaratan:</strong></p>
+                            <ul>
+                                <li>IPK minimum 3</li>
+                                <li>Rekomendasi akademik dari dosen atau guru</li>
+                                <li>Memiliki portfolio</li>
+                                <li>Formulir pendaftaran lengkap</li>
+                            </ul>
+                            <p><strong>Dokumen yang Diperlukan:</strong></p>
+                            <ul>
+                                <li>CV terbaru</li>
+                                <li>Fotokopi KTP</li>
+                                <li>Transkrip Nilai</li>
+                                <li>Pas foto terbaru</li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-12 col-lg-6">
+                    <div class="card py-2">
+                        <h3 class="subtitle text-center">Non-Akademik</h3>
+                        <div class="card-body">
+                            <p><strong>Deskripsi:</strong></p>
+                            <p>Beasiswa Non-Akademik ditujukan untuk individu yang menunjukkan bakat atau kontribusi luar biasa di luar bidang akademis. Program ini berfokus pada prestasi dalam olahraga, seni, atau kegiatan komunitas.</p>
+                            <p><strong>Persyaratan:</strong></p>
+                            <ul>
+                                <li>IPK minimum 3</li>
+                                <li>Pengalaman atau prestasi dalam bidang olahraga, seni, atau kegiatan sosial</li>
+                                <li>Formulir pendaftaran lengkap</li>
+                            </ul>
+                            <p><strong>Dokumen yang Diperlukan:</strong></p>
+                            <ul>
+                                <li>CV terbaru</li>
+                                <li>Fotokopi KTP</li>
+                                <li>Sertifikat bidang keahlian</li>
+                                <li>Pas foto terbaru</li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+</section>
